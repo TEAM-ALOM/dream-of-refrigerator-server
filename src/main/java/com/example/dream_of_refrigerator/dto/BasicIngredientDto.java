8e6d7ea233847506dcf25c60d6d271bc9fe0b99b
hwangjaehee <zh_hui@naver.com>
2024-09-14 13:56:36 +0900

    feat:재료추가페이지_재료조회(전체,카테고리,검색) 기능구현

--- src/main/java/com/example/dream_of_refrigerator/dto/BasicIngredientDto.java
+++ src/main/java/com/example/dream_of_refrigerator/dto/BasicIngredientDto.java
@@ -3,7 +3,7 @@ package com.example.dream_of_refrigerator.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter //게터
+@Getter
 @AllArgsConstructor //기본생성자
 public class BasicIngredientDto {
     private String name;
--- src/main/java/com/example/dream_of_refrigerator/repository/IngredientRepository.java
+++ src/main/java/com/example/dream_of_refrigerator/repository/IngredientRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient,String>, JpaSpecificationExecutor<Ingredient> {
-//    List<Ingredient> findByIngredientCategory_Id(Long categoryId);
+    List<Ingredient> findByCategory(String category);
 
-    // UUID로 재료 조회
-  //  List<Ingredient> findByUuid(String uuid);
 }
